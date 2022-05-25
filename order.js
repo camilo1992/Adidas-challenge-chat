80f294aa2d9eb59a1bb18ec9c01b50deb80179f4
Cristian camilo Gutierrez correa <gccc1108@hotmail.com>
2022-05-25 14:20:12 -0500

    Responsive design finished

--- order.js
+++ order.js
@@ -68,3 +68,193 @@
 // return (userIdTalkingTo) => {
 //   collection(db, "connected", userIdTalkingTo, "private-chats");
 // };
+
+// .publicChat {
+//     background: linear-gradient(
+//       to left,
+//       var(--first-colour),
+//       var(--second-colour)
+//     );
+//     /* background-color: var(--first-colour); */
+
+//     height: 73vh;
+//     width: 60vw;
+
+//     position: relative;
+//     left: 25.5vw;
+//     top: -97.5vh;
+//     right: 0;
+//     margin: 0;
+//     display: flex;
+//     flex-direction: column;
+
+//     border-radius: 5px;
+//     border-top-left-radius: 0;
+//     border-bottom-left-radius: 0;
+//     opacity: 0.8;
+
+//     z-index: 99;
+//     animation: open 2s ease-out;
+//   }
+
+//   .messageContainerRecieved {
+//     margin: 0;
+//     margin-bottom: 15px;
+//     margin-right: 0px;
+//     margin-top: 3px;
+//     display: flex;
+//     width: 80%;
+//     height: 14%;
+//     position: relative;
+
+//     text-align: left;
+//     left: 5%;
+//     flex-direction: row-reverse;
+//     font-size: 2rem;
+//   }
+
+//   .messageContainerSent {
+//     font-size: 20px;
+//     margin: 0;
+//     margin-bottom: 15px;
+//     margin-right: 0px;
+//     margin-top: 3px;
+//     display: flex;
+//     width: 80%;
+//     height: 14%;
+//     position: relative;
+
+//     /* text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.15); */
+//     /* box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.15); */
+
+//     text-align: right;
+//     left: 16%;
+//   }
+
+//   p {
+//     font-size: 3rem;
+//     margin: 1vh;
+//     height: 2vh;
+//     margin-left: 10px;
+//     font-family: "Audiowide", cursive;
+//     color: var(--fourth-colour);
+//   }
+
+//   .publicChat2 {
+//     background: linear-gradient(
+//       to left,
+//       var(--first-colour),
+//       var(--second-colour)
+//     );
+//     height: 73vh;
+//     width: 40vw;
+
+//     position: relative;
+//     left: 25.5vw;
+//     top: -97.5vh;
+//     right: 0;
+//     margin: 0;
+//     display: flex;
+//     flex-direction: column;
+
+//     border-radius: 5px;
+//     opacity: 0.8;
+
+//     z-index: 99;
+//     /* animation: shrink 0.4s ease-out; */
+
+//     border-top-left-radius: 0;
+//     border-bottom-left-radius: 0;
+//   }
+
+//   .screen {
+//     position: relative;
+//     width: 58vw;
+//     height: 48vh;
+//     background-color: var(--second-colour);
+//     margin: auto;
+//     border-radius: 5px;
+//     overflow-y: auto;
+//     overflow-x: hidden;
+//   }
+
+//   .screen::-webkit-scrollbar {
+//     display: none;
+//   }
+
+//   .buttonContainer {
+//     border-radius: 29px;
+//     height: 30%;
+//     position: relative;
+//     width: 14%;
+//     top: -7vh;
+//     left: 4%;
+//     background-color: var(--third-colour);
+//   }
+//   .form {
+//     margin: 0;
+//     padding: 0;
+//     /* position: relative; */
+//   }
+//   .textArea {
+//     font-size: 100%;
+//     font-family: "Audiowide", cursive;
+//     color: var(--first-colour);
+//     margin-left: 3%;
+//     width: 56vw;
+//     height: 12vh;
+//     position: relative;
+//     top: 0.2vh;
+//     text-align: right;
+//     background-color: var(--second-colour);
+//   }
+
+//   @media (max-width: 985px) {
+//     .publicChat {
+//       width: 80%;
+//       left: 2.5vw;
+//       top: -27.5vh;
+//     }
+//     .publicChat2 {
+//       width: 80%;
+//       left: 2.5vw;
+//       top: -27.5vh;
+//     }
+
+//     .messageContainerRecieved {
+//       font-size: 1.4rem;
+//     }
+//     .messageContainerSent {
+//       font-size: 1.4rem;
+//     }
+//     .screen {
+//       width: 60vw;
+//     }
+//   }
+
+//   @keyframes open {
+//     0% {
+//       opacity: 0;
+//       transform: translateY(-200%);
+//     }
+
+//     100% {
+//       opacity: 1;
+//       transform: translateY(0);
+//     }
+//   }
+
+//   @keyframes shrink {
+//     0% {
+//       opacity: 1;
+//       transform: scale(0.01, 1);
+//     }
+//     50% {
+//       opacity: 1;
+//       transform: scale(0.01, 1);
+//     }
+//     100% {
+//       opacity: 1;
+//       transform: scale(1, 1);
+//     }
+//   }
